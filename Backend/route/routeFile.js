d0e97635483117d0c9efc0294dcfed15094193e2
Harshit yadav <63390121+harryyd@users.noreply.github.com>
2025-01-05 01:19:37 +0530

    Update routeFile.js

--- Backend/route/routeFile.js
+++ Backend/route/routeFile.js
@@ -14,15 +14,7 @@ router.post("/resetpassword/:token" , resetPassword );
 
 
 //for access checking  
-router.get("/alternate/:id" , alternate)
 router.get("/test",verifytoken, test) ;
-// router.get("/checkAuth" ,verifytoken , checkAuth) ;
-// router.get("/getCookie" , getCookie) ;
-// router.get("/token" , getToken) ;
-
-
-
-// router.get("/mail" , mail) ;
 
 
 module.exports = router ; 
